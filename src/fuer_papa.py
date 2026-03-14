d2051446192fea05c119b53cd431fbe8f7343399
HCSchmidt <dr.h.schmidt@online.de>
2026-03-14 18:16:19 +0100

    Update fuer_papa.py
    CONFIGURATION START.py  14.3.26

--- src/fuer_papa.py
+++ src/fuer_papa.py
@@ -1,29 +1,24 @@
-"""Entrypoint for Helmut :)"""
-
 from polynome2pi.main import main
-
 # =========================
 # CONFIGURATION START
 # =========================
-
+#    Selection of the experiment
+Exp = [ "000 u d", "012 u d s", "112 u d s nucleon",
+      "111 H-atom", "112 E > 1700", "222 u d s c",
+      "333 E > 1700", "333 E < 2000",  
+    ]
 ARGS = [
-    "--sector", "nucleon",
-    # "--no-open",
-]
-
-# Examples:
-# ARGS = ["--sector", "minimal"]
-# ARGS = ["--sector", "nucleon"]
-# ARGS = ["--sector", "heavy"]
-# ARGS = ["--sector", "light"]
-
-# =======================
-# USER CONFIGURATION END
-# =======================
+        "--sector", Exp[6]
+    ]
+# select Charge  [" ","1","2/3","1/3","0","-1/3","-2/3","-1","+-1"] 
+ARGC = [
+        "--Charge", " ",     
+    ]
+# select H Atom  ["H","N"] or [" "," "] to see the Proton
+ARGH = [
+        "--HAtom", "H", "N"   
+    ]
 
 if __name__ == "__main__":
-    main(["--sector", "minimal"])
-    main(["--sector", "light"])
-    main(["--sector", "broad"])
-    main(["--sector", "nucleon"])
-    main(["--sector", "heavy"])
+        print("Running with arguments:", ARGS, ARGC, ARGH)
+        main(ARGS, ARGC, ARGH)
